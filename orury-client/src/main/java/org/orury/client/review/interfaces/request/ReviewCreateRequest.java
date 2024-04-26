48e5318843e787c324fe873c6ee015ba21074e51
Hyungjoon Cho <selcon1996@gmail.com>
2024-04-26 10:17:35 +0900

    Feat : ReviewCreateRequest 필드 추가

--- orury-client/src/main/java/org/orury/client/review/interfaces/request/ReviewCreateRequest.java
+++ orury-client/src/main/java/org/orury/client/review/interfaces/request/ReviewCreateRequest.java
@@ -1,6 +1,7 @@
 package org.orury.client.review.interfaces.request;
 
 import org.orury.domain.gym.domain.dto.GymDto;
+import org.orury.domain.review.domain.dto.Difficulty;
 import org.orury.domain.review.domain.dto.ReviewDto;
 import org.orury.domain.user.domain.dto.UserDto;
 
@@ -9,10 +10,13 @@ import java.util.List;
 public record ReviewCreateRequest(
         String content,
         float score,
-        Long gymId
+        Long gymId,
+        String description,
+        Difficulty difficulty,
+        List<String> tags
 ) {
-    public static ReviewCreateRequest of(String content, float score, Long gymId) {
-        return new ReviewCreateRequest(content, score, gymId);
+    public static ReviewCreateRequest of(String content, float score, Long gymId, String description, Difficulty difficulty, List<String> tags) {
+        return new ReviewCreateRequest(content, score, gymId, description, difficulty, tags);
     }
 
     public ReviewDto toDto(UserDto userDto, GymDto gymDto) {
---
+++ orury-domain/src/main/java/org/orury/domain/review/domain/dto/Difficulty.java
@@ -0,0 +1,32 @@
+package org.orury.domain.review.domain.dto;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Difficulty {
+    RED("빨강"),
+    ORANGE("주황"),
+    YELLOW("노랑"),
+    GREEN("초록"),
+    BLUE("파랑"),
+    NAVY("남색"),
+    PURPLE("보라"),
+    GRAY("회색"),
+    BLACK("검정"),
+    COMPI("컴피");
+
+    private final String description;
+
+    @JsonCreator
+    public static Difficulty getEnumFromValue(String value) {
+        for (Difficulty difficulty : Difficulty.values()) {
+            if (difficulty.name().equals(value)) {
+                return difficulty;
+            }
+        }
+        return null;
+    }
+}
